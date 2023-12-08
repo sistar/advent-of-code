501a97f08e2a2b7fb77b49658400f570f669eef8
Ralf Sigmund <ralf.sigmund@gmail.com>
2023-12-08 22:55:31 +0100

    parallelization with rayon and generate the maps used to transform only once (instead of per seed). crossed-fingers

--- aoc/2023/day-05/src/bin/part-1.rs
+++ aoc/2023/day-05/src/bin/part-1.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use rayon::prelude::*;
 use std::collections::HashMap;
 fn main() {
     let input = include_str!("../input.txt");
@@ -9,34 +10,82 @@ fn main() {
     let result = part_2(input);
     println!("Result part 2: {}", result);
 }
-fn range_seeds(start: u64, len: u64) -> Vec<u64> {
-    (start..start + len).collect_vec()
+fn range_seeds(start: u64, len: u64) -> impl Iterator<Item = u64> {
+    start..start + len
 }
-fn vec_range_seeds(seed_ranges: &Vec<u64>) -> Vec<u64> {
-    seed_ranges
-        .chunks(2)
-        .into_iter()
-        .flat_map(|chunk| {
-            if let [start, len] = chunk {
-                range_seeds(*start, *len)
-            } else {
-                vec![]
-            }
-        })
-        .collect_vec()
+fn vec_range_seeds(seed_ranges: &Vec<u64>) -> impl Iterator<Item = u64> + '_{
+    seed_ranges.chunks(2).map(|chunk| (chunk[0],chunk[1]))
+        .flat_map(|chunk| range_seeds(chunk.0, chunk.1))
 }
 fn part_2(input: &str) -> u64 {
-    let game = parse_input(input, vec_range_seeds);
-    *game.locations().iter().min().unwrap()
+    let game = parse_input(input);
+    
+    let chunks = game.seed_source.chunks(2).map(|chunk| (chunk[0],chunk[1])).collect_vec();
+    chunks.par_iter().map(|chunk| {
+        let seeds = range_seeds(chunk.0, chunk.1);
+        game.min_location(seeds)
+    }).min().unwrap()
 }
 
 fn part_1(input: &str) -> u64 {
-    let game = parse_input(input, |seeds| seeds.clone());
-    *game.locations().iter().min().unwrap()
+    let game = parse_input(input);
+    game.min_location(game.seed_source.clone().into_iter())
 }
 struct Game {
-    seeds: Vec<u64>,
     maps: HashMap<&'static str, Vec<Vec<u64>>>,
+    seed_source: Vec<u64>,
+    transformer_maps: HashMap<&'static str, Vec<Range>>,
+}
+impl Game {
+    fn new(maps: HashMap<&'static str, Vec<Vec<u64>>>, seed_source: Vec<u64>) -> Self {
+        let mut transformer_maps: HashMap<&'static str, Vec<Range>> = HashMap::new();
+        for map in X_TO_Y.iter() {
+            let t_m = maps
+            .get(map)
+            .unwrap()
+            .iter()
+            .map(|x| {
+                let (dest_range_start, source_range_start, range_length) = (x[0], x[1], x[2]);
+                Range::new(
+                    source_range_start,
+                    source_range_start + range_length,
+                    dest_range_start,
+                )
+            })
+            .collect::<Vec<Range>>();
+            transformer_maps.insert(map, t_m);
+        }
+        Self {
+            maps,
+            seed_source,
+            transformer_maps,
+        }
+    }
+    fn do_map(&self, map: &str, input: u64) -> u64 {
+        let map = self.transformer_maps.get(map).unwrap();
+        match map.iter().find(|t| t.contains(input)) {
+            Some(t) => t.destination(input),
+            None => input,
+        }
+    }
+    fn do_maps(&self, input: u64) -> u64 {
+        let mut result = input;
+        for map in X_TO_Y.iter() {
+            result = self.do_map(map, result);
+        }
+        result
+    }
+    fn min_location(&self,seeds: impl Iterator<Item = u64>) -> u64 {
+            seeds
+            .map(|seed| self.do_maps(seed))
+            .min()
+            .unwrap()
+    }
+    fn locations(&self, seeds: impl Iterator<Item = u64>) -> Vec<u64> {
+            seeds
+            .map(|seed| self.do_maps(seed))
+            .collect::<Vec<u64>>()
+    }
 }
 struct Range {
     start: u64,
@@ -54,42 +103,6 @@ impl Range {
         self.dest + input - self.start
     }
 }
-impl Game {
-    fn do_map(&self, map: &str, input: u64) -> u64 {
-        let transformer_maps: Vec<Range> = self
-            .maps
-            .get(map)
-            .unwrap()
-            .iter()
-            .map(|x| {
-                let (dest_range_start, source_range_start, range_length) = (x[0], x[1], x[2]);
-                Range::new(
-                    source_range_start,
-                    source_range_start + range_length,
-                    dest_range_start,
-                )
-            })
-            .collect();
-        match transformer_maps.iter().find(|t| t.contains(input)) {
-            Some(t) => t.destination(input),
-            None => input,
-        }
-    }
-    fn do_maps(&self, input: u64) -> u64 {
-        let mut result = input;
-        for map in X_TO_Y.iter() {
-            result = self.do_map(map, result);
-        }
-        result
-    }
-    fn locations(&self) -> Vec<u64> {
-        self.seeds
-            .iter()
-            .map(|seed| self.do_maps(*seed))
-            .collect::<Vec<u64>>()
-    }
-}
-
 const X_TO_Y: [&str; 7] = [
     "seed-to-soil map:",
     "soil-to-fertilizer map:",
@@ -109,13 +122,9 @@ fn parse_seeds(input: &str) -> Vec<u64> {
         .map(|x| x.parse::<u64>().unwrap())
         .collect::<Vec<u64>>()
 }
-fn parse_input<F>(input: &str, seeds_interpreter: F) -> Game
-where
-    F: Fn(&Vec<u64>) -> Vec<u64>,
-{
+fn parse_input(input: &str) -> Game {
     let mut inp = input.lines();
-    let seeds = seeds_interpreter(&parse_seeds(inp.next().unwrap()));
-
+    let seed_source = parse_seeds(inp.next().unwrap());
     let mut maps: HashMap<&'static str, Vec<Vec<u64>>> = HashMap::new();
     for k in X_TO_Y.iter() {
         maps.insert(k, vec![]);
@@ -135,9 +144,9 @@ where
             maps.get_mut(k).unwrap().push(data);
         }
     }
-    println!("{:?}", maps);
+    //println!("{:?}", maps);
 
-    Game { seeds, maps }
+    Game::new(maps, seed_source) 
 }
 
 #[cfg(test)]
@@ -147,7 +156,7 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        let game = parse_input(include_str!("../sample.txt"), |seeds| seeds.clone());
+        let game = parse_input(include_str!("../sample.txt"));
 
         let input = 79;
         let result = game.do_map("seed-to-soil map:", input);
@@ -179,19 +188,19 @@ mod tests {
         let result = game.do_maps(input);
         assert_eq!(result, 43);
 
-        let result = game.locations();
+        let result = game.locations(game.seed_source.clone().into_iter());
         let expected = vec![82, 43, 86, 35];
         assert_eq!(result, expected);
 
         let expected = 35;
-        let result = *game.locations().iter().min().unwrap();
+        let result = game.min_location(game.seed_source.clone().into_iter());
         assert_eq!(result, expected);
     }
 
     #[test]
     fn test_parse_seeds() {
         let input = "seeds: 79 14 55 13";
-        let result = vec_range_seeds(&parse_seeds(input));
+        let result = vec_range_seeds(&parse_seeds(input)).collect_vec();
         let expected = vec![
             79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 55, 56, 57, 58, 59, 60, 61, 62,
             63, 64, 65, 66, 67,
